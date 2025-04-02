878cd50dff2130fc45cc71a4bbf34c8caa50ec0b
zhiro <egor.zhir.natife@gmail.com>
2025-04-02 21:49:04 +0300

    Initial commit

---
+++ common/src/main/java/com/zhiro/lecture/plugin/convention/core/common/MyClass.kt
@@ -0,0 +1,4 @@
+package com.zhiro.lecture.plugin.convention.core.common
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lecture Plugin Convention"
+include(":app")
+include(":domain")
+include(":data")
+include(":feature:auth")
+include(":feature:dashboard")
+include(":common")
